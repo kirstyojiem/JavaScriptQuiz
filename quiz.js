4071eb9bb2c628cb423c23a5f3b946982a8e24d5
Kirsty Ojiem <kirstyojiem@Kirstys-MacBook-Air.local>
2020-10-30 15:50:45 +0000

    edited javascript

--- quiz.js
+++ quiz.js
@@ -1,38 +1,28 @@
-var myscore = 0;
-var missed = 0;
-var answered = 0;
+console.log("executing javascript")
+// $("#prospects_form").submit(function(e) {
+// e.preventDefault();
+// });
+const submitAnswers=(e)=>{
+e.preventDefault();   
+console.log("submit answers")
 
-var question1 = document.getElementById("myCheck");
-var myl = document.getElementById("myCheck1");
-var myl2 = document.getElementById("myCheck2");
-var myl3 = document.getElementById("myCheck3");
+var total = 0;
+var score = 0;
+console.log(document.forms)
+// var q1option = document.forms["quizForm"]["q1"].value;
+// console.log("q1",q1option)
+// var q2option = document.forms["quizForm"]["q2"].value;
+// var q3option = document.forms["quizForm"]["q3"].value;
+// var q4option = document.forms["quizForm"]["q4"].value;
 
-var array1 = [myl, myl1, myl2, myl3];
+// var answers = ["1a","2b","3b","4c"];
+// for(i=1; i<=total; i++) {
+//     if(eval('q'+i)=== answers[i-1]) {
+//         score++;
+//     }
+// }
 
-function myFunction() {
-    
-    for (i=0; i<=array1. length;i++) {
-        if (array1[i].checked == true) {
-            myscore += 1;
-        }
-
-        var obtained = myscore;
-        var total = 4;
-        var percent = obtained*100/total;
-        var finalscore = percent+ "%";
-
-        document.getElementById("demo").innerHTML = "You scored " + myscore + "Out of 4." +" <b>Grade:</b>"+ finalscore;
-            document.querySelector(".finalscore").className += "hide";
-    
-    }
-    function uncheck() {
-
-        for (i=0; i<array1.lengt;i++) {
-            if (array1[i].checked == false) {
-                missed += 1;
-            }
-            document.getElementById("missed").innerHTML = "you missed" + missed + "Out of 4";
-            document.querySelector(".unchecked").className += "hide";
-        }
-    }
+var results = document.getElementById('results');
+results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
+return false;
 }
