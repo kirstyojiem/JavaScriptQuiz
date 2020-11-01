b340d1a34c0d30d151d15e2a1e5ab09be35051f0
Kirsty Ojiem <kirstyojiem@Kirstys-Air.lan>
2020-11-01 12:03:33 +0000

    JavaScript debugging

--- quiz.js
+++ quiz.js
@@ -2,25 +2,36 @@ console.log("executing javascript")
 // $("#prospects_form").submit(function(e) {
 // e.preventDefault();
 // });
-const submitAnswers=(e)=>{
-e.preventDefault();   
-console.log("submit answers")
+const submitAnswers = (e) => {
 
-var total = 0;
+var submittedAnswers = [q1, q2, q3, q4];
+var answers = ["1a","2b","3b","4c"];
+
+var total = answers.length;
 var score = 0;
-console.log(document.forms)
-// var q1option = document.forms["quizForm"]["q1"].value;
-// console.log("q1",q1option)
-// var q2option = document.forms["quizForm"]["q2"].value;
-// var q3option = document.forms["quizForm"]["q3"].value;
-// var q4option = document.forms["quizForm"]["q4"].value;
 
-// var answers = ["1a","2b","3b","4c"];
-// for(i=1; i<=total; i++) {
-//     if(eval('q'+i)=== answers[i-1]) {
-//         score++;
-//     }
-// }
+var q1 = document.forms["quizForm"]["q1"].value;
+var q2 = document.forms["quizForm"]["q2"].value;
+var q3 = document.forms["quizForm"]["q3"].value;
+var q4 = document.forms["quizForm"]["q4"].value;
+
+console.log("answer q1: ", typeof q1); 
+
+
+
+for(i = 0; i <= 4; i++) {
+    submittedAnswers[i] = "q" + submittedAnswers[i];
+    console.log(submitAnswers[i])
+    if(submittedAnswers[i] == answers[i]) {
+        score++;
+    }
+}
+
+
+
+
+
+//on submit -> wgere are the answer stored
 
 var results = document.getElementById('results');
 results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
